b5cffdc1ccd21f12524a3bf7090917d6002009c5
sijilsasidharan <sijilsasidharan@gmail.com>
2024-12-04 16:21:21 +0530

    Config changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,18 @@ export default {
   theme: {
     extend: {},
     fontFamily: {
-      sans: ['Poppins', 'sans-serif']
-    }
+      sans: ["Poppins", "sans-serif"],
+    },
   },
   plugins: [],
-}
-
+  safelist: [
+    "bg-slate-300",
+    "text-slate-900",
+    "bg-sky-300",
+    "text-sky-900",
+    "bg-blue-300",
+    "text-blue-900",
+    "bg-green-300",
+    "text-green-900",
+  ],
+};
