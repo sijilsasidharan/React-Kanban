d15c402444cbec743aa58ccd0809944e28a89eb4
sijilsasidharan <sijilsasidharan@gmail.com>
2024-12-07 23:01:30 +0530

    type updates

--- src/types/column.type.ts
+++ src/types/column.type.ts
@@ -1,7 +1,9 @@
+import { HEX } from "./color";
 import { Task } from "./task.type";
 
 export interface Column {
-    id: string;
-    name: string;
-    tasks?: Task[];
+  id: string;
+  name: string;
+  tasks?: Task[];
+  color: HEX;
 }
