acb7f36ecb50a6ee7ba5ebf6d8f577612fc8645b
sijilsasidharan <sijilsasidharan@gmail.com>
2024-12-16 23:57:42 +0530

    Side bar udates

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import KanbanBoard from './pages/KanbanBoard'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import KanbanBoard from "./pages/KanbanBoard";
+import SideBar from "./components/core/SideBar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <KanbanBoard />
+      <SideBar />
+      {/* <KanbanBoard /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
