8e167342b301bfc1e3211a22635da3f6e69802c2
sijilsasidharan <sijilsasidharan@gmail.com>
2024-12-18 23:45:38 +0530

    shadcn - components

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,41 +1,57 @@
-import React, { MouseEventHandler } from "react";
+import * as React from "react"
+import { Slot } from "@radix-ui/react-slot"
+import { cva, type VariantProps } from "class-variance-authority"
 
-interface ButtonProps extends React.PropsWithChildren {
-  primary?: boolean;
-  type?: "button" | "submit";
-  className?: string;
-  onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
-  icon?: React.ReactNode;
-  disabled?: boolean;
+import { cn } from "@/lib/utils"
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  {
+    variants: {
+      variant: {
+        default:
+          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+        destructive:
+          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
+        outline:
+          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
+        secondary:
+          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
+        ghost: "hover:bg-accent hover:text-accent-foreground",
+        link: "text-primary underline-offset-4 hover:underline",
+      },
+      size: {
+        default: "h-9 px-4 py-2",
+        sm: "h-8 rounded-md px-3 text-xs",
+        lg: "h-10 rounded-md px-8",
+        icon: "h-9 w-9",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+)
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean
 }
 
-const Button: React.FC<ButtonProps> = ({
-  type = "button",
-  className,
-  onClick,
-  children,
-  icon,
-  disabled,
-}) => {
-  return (
-    <button
-      type={type}
-      disabled={disabled}
-      className={`bg-zinc-900
-       text-white
-        ${icon ? "px-2" : "px-6"}
-        py-2
-        border-2
-       border-zinc-700
-        rounded-lg
-        hover:ring-2 hover:ring-red-600 hover:bg-zinc-800 duration-100 hover:border-zinc-900
-        disabled:text-zinc-600 disabled:border-zinc-500 disabled:shadow-none disabled:hover:ring-0 disabled:hover:bg-zinc-900
-         ${className}`}
-      onClick={onClick}
-    >
-      {icon} {children}
-    </button>
-  );
-};
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button"
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
+Button.displayName = "Button"
 
-export default Button;
+export { Button, buttonVariants }
--- src/components/ui/icons/index.tsx
+++
@@ -1,3 +0,0 @@
-export * from "./Add.icon";
-export * from "./Close.icon";
-export * from "./Delete.icon";
