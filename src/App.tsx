f90f3d3c85ac05c94886f403e24abe0fe1881a39
sijilsasidharan <sijilsasidharan@gmail.com>
2024-12-20 23:03:56 +0530

    Configuring router

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import "./App.css";
 import Layout from "./components/core/Layout";
-import { ThemeProvider } from "./providers/theme-providers";
 import Login from "./pages/Login";
+import NotFound from "./components/Error/NotFound";
+import { ThemeProvider } from "./context/theme-providers";
+import ProtectedRoutes from "./components/common/ProtectedRoutes";
+import Dashboard from "./pages/Dashboard";
+import { Inbox, Settings } from "lucide-react";
+import KanbanBoard from "./pages/KanbanBoard";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route
-          path="/dashboard"
-          element={
-            <Layout>
-              <h1>H1</h1>
-            </Layout>
-          }
-        />
+        <Route path="/" element={<Layout />} />
+        <Route path="/login" element={<Login />} />
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/" element={<Layout />}>
+            <Route index path="/dashboard" element={<Dashboard />} />
+            <Route path="/inbox" element={<Inbox />} />
+            <Route path="/boards" element={<KanbanBoard />} />
+            <Route path="/settings" element={<Settings />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </ThemeProvider>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router";
 
 import "./index.css";
 import App from "./App.tsx";
@@ -8,9 +8,6 @@ import App from "./App.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
       <App />
     </BrowserRouter>
   </StrictMode>
