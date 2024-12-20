46b1032f284ad36a0b67b873450ef9fe33af9bbb
sijilsasidharan <sijilsasidharan@gmail.com>
2024-12-20 23:03:13 +0530

    config changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import * as path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import * as path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({ tsDecorators: true })],
   resolve: {
-    alias: [{
-      find: '@',
-      replacement: path.resolve(__dirname, 'src')
-    }]
-  }
-})
+    alias: [
+      {
+        find: "@",
+        replacement: path.resolve(__dirname, "src"),
+      },
+    ],
+  },
+});
