0c866e725997c6514daa446d1286abe459830317
sijilsasidharan <sijilsasidharan@gmail.com>
2024-12-22 20:28:42 +0530

    User dropdown

--- src/features/auth/UserDropdown.tsx
+++ src/features/auth/UserDropdown.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
-import { LogOut, Moon, User } from "lucide-react";
+import { LogOut, User } from "lucide-react";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
 } from "@/components/ui/dropdown-menu";
 import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import { useNavigate } from "react-router";
 
 const UserDropdown = () => {
+  const navigate = useNavigate();
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -22,7 +24,7 @@ const UserDropdown = () => {
           <User className="h-[1.2rem] w-[1.2rem]" />
           <span>User Details</span>
         </DropdownMenuItem>
-        <DropdownMenuItem>
+        <DropdownMenuItem onClick={() => navigate("/login")}>
           <div className="row-center gap-2">
             <LogOut className="h-[1.2rem] w-[1.2rem]" />
             <span>Sign out</span>
